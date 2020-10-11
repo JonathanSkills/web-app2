f0d0e13d4904ad5d4d26935f0bdb825869ba733f
Ubuntu <ubuntu@ip-172-31-16-49.ec2.internal>
2020-10-11 09:53:03 +0000

    ya está profe, tanto dev como prod

---
+++ services/users/manage.py
@@ -0,0 +1,42 @@
+# services/users/manage.py
+
+
+import sys
+import unittest
+
+from flask.cli import FlaskGroup
+
+from project import create_app, db  # nuevo
+from project.api.models import User  # nuevo
+
+app = create_app()  # nuevo
+cli = FlaskGroup(create_app=create_app)  # nuevo
+
+
+@cli.command("recreate_db")
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+
+@cli.command()
+def test():
+    """Ejecutar los tests sin code coverage"""
+    tests = unittest.TestLoader().discover("project/tests", pattern="test*.py")
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    sys.exit(result)
+
+
+@cli.command("seed_db")
+def seed_db():
+    """Siembra la base de datos."""
+    db.session.add(User(username="cromero", email="kalosrom@gmail.com"))
+    db.session.add(User(username="carlos.romero", email="jonathanromero@upeu.edu.pe"))
+    db.session.commit()
+
+
+if __name__ == "__main__":
+    cli()
