e30f71cfc66a01e4470fda91ec5d13ea89f14bad
JonathanSkills <jonathanromero@upeu.edu.pe>
2020-12-01 19:20:59 -0500

    pruebas React + List + Add

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';  // new
 import UsersList from './components/UsersList';
+import AddUser from './components/AddUser';
 
 class App extends Component {
   constructor() {
@@ -26,10 +27,12 @@ class App extends Component {
       <section className="section">
         <div className="container">
           <div className="columns">
-            <div className="column is-one-third">
+            <div className="column is-half">  {/* new */}
               <br/>
               <h1 className="title is-1">All Users</h1>
               <hr/><br/>
+              <AddUser/>  {/* nuevo */}
+              <br/><br/>  {/* nuevo */}
               <UsersList users={this.state.users}/>
             </div>
           </div>
